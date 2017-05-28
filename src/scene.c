a7471af6391bb0f40bbd9051135b5dd90a7cba69
sam <ssaammp@gmail.com>
2017-05-28 16:29:03 -0500

    only update existant scene, quit if returns false

--- src/scene.c
+++ src/scene.c
@@ -14,7 +14,7 @@ void scene_run(Scene s) {
                 break;
             }
         }
-        s.update(&s);
+        if (s.update && !s.update(&s)) s.quit = 1;
         SDL_SetRenderDrawColor(s.it.rdr, 0,0,0,255);
         SDL_RenderClear(s.it.rdr);
         draw_all(&s.draw, s.it.rdr);
