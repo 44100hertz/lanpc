5ce63b6ef4aa01f206c577bfa2568692904cfb94
sam <ssaammp@gmail.com>
2017-05-29 08:04:02 -0500

    -1's that don't belong in input

--- src/input.c
+++ src/input.c
@@ -6,8 +6,8 @@ void input_update(Input* input)
     const Uint8* kb = SDL_GetKeyboardState(NULL);
     Buttons* bind = &input->bind;
     Buttons* hist = &input->hist;
-    for(int i=INPUTS-1; i--;) ((int*)hist)[i] <<= 1;
-    for(int i=BUTTONS-1; i--;) ((int*)hist)[i] += kb[((int*)bind)[i]];
+    for(int i=INPUTS; i--;) ((int*)hist)[i] <<= 1;
+    for(int i=BUTTONS; i--;) ((int*)hist)[i] += kb[((int*)bind)[i]];
 
     hist->du += (kb[bind->du] && !kb[bind->dd]);
     hist->dd += (kb[bind->dd] && !kb[bind->du]);
